1b8ee3f6cd44ec87ff41043670c8f65af49fd6aa
Anukit <anukit084@hotmail.com>
2021-05-17 20:15:59 +0700

    SSS

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'AAAA',
+              'SSSSSSSSSSSSSSSS',
             ),
             Text(
               '$_counter',
